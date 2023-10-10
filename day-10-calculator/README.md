a11f1a9c3d5bb5718c6b2540677b760dacb3b3cf
Roberto Junior <gsn.roberto@gmail.com>
2023-10-09 21:35:07 -0300

    Add Day 10 Project: Calculator

---
+++ day-10-calculator/README.md
@@ -0,0 +1,17 @@
+# Day 10 Project: Calculator
+
+## Concept
+
+This program works as a simple calculator that works as follows:
+
+1. Prompt the user for a number;
+2. Prompt the user for one of four options of operations;
+3. Prompt the user for the next number;
+4. Present the result of the operation;
+5. Prompt the user to continue calculating using the previous result as the first number or to close the program;
+
+## Resources
+
+### Data Structures
+
+- [Python 3 Dictionaries](https://docs.python.org/3/tutorial/datastructures.html#dictionaries)
---
+++ day-10-calculator/main.py
@@ -0,0 +1,52 @@
+from art import logo
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+}
+
+
+def calculate():
+    print(logo)
+
+    num1 = float(input("What's the first number? "))
+
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operator = input("Pick an operation: ")
+        num2 = float(input("What's the next number? "))
+        calc_function = operations[operator]
+        result = calc_function(num1, num2)
+
+        print(f"{num1} {operator} {num2} = {result}")
+
+        if input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = result
+        else:
+            should_continue = False
+            calculate()
+
+
+calculate()
