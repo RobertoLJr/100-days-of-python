61fbc0290653d7558633e4f177818982d2b0d633
Roberto Junior <gsn.roberto@gmail.com>
2023-12-13 17:36:31 -0300

    Add Day 38 Project: Workout Tracking

---
+++ day-038-project-workout-tracking/README.md
@@ -0,0 +1,33 @@
+# Day 38 Project: Workout Tracking
+
+## Concept
+
+This program makes use of the Nutritionix API and the Sheety API with Google Sheets to work as a place to record
+workouts using natural language. The user should first make sure to have valid keys in Nutritionix and Sheet before
+using this program. Then, they can execute it as the following:
+
+### Requirements
+
+1. Nutritionix API App ID and Key (see resources).
+2. A sheet in Google Sheets.
+3. Sheety API data: username, project name, sheet name, and authentication credentials (if any).
+
+### Usage
+
+1. Provide the App ID and the App Key for Nutritionix as environment variables.
+2. Provide the Sheety data as environment variables (username, project, sheet and token (if any)).
+3. Then, whenever executing the program, the user can use natural language to record workout sessions. For example, when prompted for
+`Tell me which exercises you did:`, the user can type `swam for 1 hour` or `walked for 30 minutes and ran for 30 minutes`.
+
+After the input, them program should update the sheet with information about Date, Time, Exercise, Duration and Calories, automatically.
+
+## Resources
+
+### APIs
+
+- [Nutritionix API Guide](https://developer.syndigo.com/docs/nutritionix-api-guide)
+- [Sheety API Documentation](https://v2.sheety.co/docs/getting-started)
+
+### Miscellanea
+
+- [More about Nutritionix](https://www.nutritionix.com/)
---
+++ day-038-project-workout-tracking/main.py
@@ -0,0 +1,50 @@
+from datetime import *
+import os
+import requests
+
+NUTRITIONIX_ID = os.environ.get("NUTRITIONIX_ID")
+NUTRITIONIX_KEY = os.environ.get("NUTRITIONIX_KEY")
+
+SHEETY_USERNAME = os.environ.get("SHEETY_USERNAME")
+SHEETY_PROJECT = os.environ.get("SHEETY_PROJECT")
+SHEETY_SHEET = os.environ.get("SHEETY_SHEET")
+SHEET_ENDPOINT = f"https://api.sheety.co/{SHEETY_USERNAME}/{SHEETY_PROJECT}/{SHEETY_SHEET}"
+
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+
+# Get data from Nutritionix using user input as natural language
+nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+user_input = input("Tell me which exercises you did: ")
+
+HEADER = {
+    "x-app-id": NUTRITIONIX_ID,
+    "x-app-key": NUTRITIONIX_KEY
+}
+
+parameters = {
+    "query": user_input
+}
+
+response = requests.post(url=nutritionix_endpoint, json=parameters, headers=HEADER)
+response.raise_for_status()
+data = response.json()
+
+# Add rows to a Google Sheet via Sheety API
+for exercise in data["exercises"]:
+    headers = {
+        "Authorization": f"Bearer {BEARER_TOKEN}"
+    }
+
+    sheety_payload = {
+        "workout": {
+            "date": str(datetime.now().strftime("%d/%m/%Y")),
+            "time": str(datetime.now().strftime("%X")),
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    response = requests.post(url=SHEET_ENDPOINT, json=sheety_payload, headers=headers)
+    response.raise_for_status()
